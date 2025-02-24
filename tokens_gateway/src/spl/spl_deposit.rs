182a0cdd1afc93cfa1f725fe81825131dc9d561a
abiiralbhattarai <abiiralbhattarai@gmail.com>
2025-02-24 10:35:39 +0545

    feat(contracts): Implement spl token update deposit method and update program Implement spl token update deposit method and update program

--- tokens_gateway/src/spl/spl_deposit.rs
+++ tokens_gateway/src/spl/spl_deposit.rs
@@ -1,5 +1,9 @@
 use crate::core::error::ProgramCustomError;
 use crate::core::state::{SplTokensVaultData, TokenDecimalMappings};
+use spl_token::solana_program::program_pack::Pack;
+use spl_token::instruction as token_instruction;
+// use crate::utils::ethereum::is_valid_ethereum_address;
+use spl_token::state::Account as TokenAccount;
 use crate::utils::ethereum_checks::is_valid_ethereum_address;
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
@@ -8,14 +12,14 @@ use solana_program::{
     msg,
     program::invoke,
     program_error::ProgramError,
-    pubkey::Pubkey,
     system_instruction,
 };
+use solana_program::pubkey::Pubkey;
+use spl_token::instruction;
 use spl_token::{
     instruction::transfer_checked,
     state::{Account, Mint},
 };
-// use crate::utils::ethereum::is_valid_ethereum_address;
 
 pub fn spl_tokens_deposit(
     program_id: &Pubkey,
@@ -54,10 +58,12 @@ pub fn spl_tokens_deposit(
     if !is_valid_ethereum_address(&twine_receiver)? {
         return Err(ProgramCustomError::InvalidArgument.into());
     }
-    // let user_balance = spl_token::state::Account::unpack(&user_token_account.data.borrow())?.amount;
-    // if user_balance < amount {
-    //     return Err(ProgramCustomError::InsufficientFundsForTransfer.into());
-    // }
+    let user_token_data = TokenAccount::unpack(&user_token_account.data.borrow())
+        .map_err(|_| ProgramError::InvalidAccountData)?;
+    if user_token_data.amount < amount {
+        msg!("User token account has insufficient funds");
+        return Err(ProgramCustomError::InsufficientFundsForTransfer.into());
+    }
     let token_decimal_mappings =
         TokenDecimalMappings::try_from_slice(&token_decimal_mappings.data.borrow())?;
     let decimal_mapping = token_decimal_mappings
@@ -65,23 +71,25 @@ pub fn spl_tokens_deposit(
         .ok_or(ProgramCustomError::TokenMappingNotFound)?;
 
     //todo: token decimal mapping checks and conversions
-    // let transfer_instruction = token_instruction::transfer(
-    //     token_program.key,
-    //     user_token_account.key,
-    //     spl_tokens_vault.key,
-    //     user.key,
-    //     &[],
-    //     amount,
-    // )?;
-    // invoke(
-    //     &transfer_instruction,
-    //     &[
-    //         user_token_account.clone(),
-    //         spl_tokens_vault.clone(),
-    //         user.clone(),
-    //         token_program.clone(),
-    //     ],
-    // )?;
+
+    let transfer_instruction = token_instruction::transfer(
+        token_program.key,
+        user_token_account.key,
+        spl_tokens_vault.key,
+        user.key,
+        &[],
+        amount,
+    )?;
+
+    invoke(
+        &transfer_instruction,
+        &[
+            user_token_account.clone(),
+            spl_tokens_vault.clone(),
+            user.clone(),
+            token_program.clone(),
+        ],
+    )?;
     // let mut spl_tokens_vault_data =
     // SplTokensVaultData::try_from_slice(&spl_tokens_vault_data.data.borrow())
     //     .map_err(|_| ProgramError::InvalidAccountData)?;
---
+++ tokens_gateway/src/utils/spl_tokens_impl.rs
@@ -0,0 +1,29 @@
+use crate::core::state::{SplTokensVaultData,TokenDepositData};
+use solana_program::program_error::ProgramError;
+use crate::core::error::ProgramCustomError;
+use solana_program::pubkey::Pubkey;
+
+impl SplTokensVaultData {
+    pub(crate) fn update_deposit(
+        &mut self,
+        minted_token_id: Pubkey,
+        amount: u64,
+    ) -> Result<(), ProgramError> {
+        if let Some(entry) = self
+            .total_deposited_amount
+            .iter_mut()
+            .find(|e| e.token_id == minted_token_id)
+        {
+            entry.amount = entry
+                .amount
+                .checked_add(amount)
+                .ok_or(ProgramError::from(ProgramCustomError::Overflow))?;
+        } else {
+            self.total_deposited_amount.push(TokenDepositData {
+                token_id: minted_token_id,
+                amount,
+            });
+        }
+        Ok(())
+    }
+}
