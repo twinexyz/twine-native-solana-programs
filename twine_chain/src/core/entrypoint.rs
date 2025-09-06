e5bc8dff50868c09e705cf1762e7e1ad26c281dc
abiiralbhattarai <abiiralbhattarai@gmail.com>
2025-09-06 16:25:20 +0545

    fix: fix code

--- twine_chain/src/core/entrypoint.rs
+++ twine_chain/src/core/entrypoint.rs
@@ -1,11 +1,13 @@
-#![allow(unexpected_cfgs)]
 //! Program entrypoint
+#[allow(unused)]
 use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
 };
 
+#[cfg(not(feature = "no-entrypoint"))]
 entrypoint!(process_instruction);
 
+#[allow(unused)]
 fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
